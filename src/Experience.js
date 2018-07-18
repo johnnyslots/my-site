dfecb785f989876a08ae1f660afbbd82dfb29b0d
Yoni Slotwinere <yonislots@gmail.com>
2018-07-18 16:59:09 -0400

    need to clearfix intro image

--- src/Experience.js
+++ src/Experience.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon } from 'semantic-ui-react'
+import { Image, Reveal, Button, Item } from 'semantic-ui-react'
+import Fade from 'react-reveal/Fade';
 
 const Experience = () => {
   return (
-    <div className="experience-container">
-      <h1> -- Experience -- </h1>
-      <Card>
-        <Image src='/images/avatar/large/matthew.png' />
-        <Card.Content>
-          <Card.Header>Fullstack Academy of Code</Card.Header>
-          <Card.Meta>
-            <span className='date'>Software Engineering Teaching Fellow</span>
-          </Card.Meta>
-          <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
-        </Card.Content>
-      </Card>
-      <div>
-        <h2>Fullstack Academy of Code</h2>
-        <h3>Software Engineering Teaching Fellow</h3>
-      </div>
+    <div  className="experience-container">
+      <h2>Experience</h2>
+        <div className="card-left">
+          <Fade cascade right>
+            <Card fluid>
+              <Card.Content>
+                <Image floated='right' size='mini' src='https://pbs.twimg.com/profile_images/694191024416112642/VtJUhbKk_400x400.png' />
+                <Card.Header>Fullstack Academy of Code</Card.Header>
+                <Card.Meta>Software Engineering Teaching Fellow</Card.Meta>
+                <Card.Description>
+                - Provide coaching and technical instruction to 7 students as their assigned teaching fellow <br />
+                - Support students in need of troubleshooting assistance and review of programming concepts during workshops <br />
+                - Lead technical and behavioral admissions interviews for prospective students
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Fade>
+        </div>
+        <div className="card-container card-right">
+          <Fade left>
+            <Card fluid>
+              <Card.Content>
+                <Image floated='right' size='mini' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Uber_App_Icon.svg/1200px-Uber_App_Icon.svg.png' />
+                <Card.Header>Uber</Card.Header>
+                <Card.Meta>Regional Greenlight Program Specialist</Card.Meta>
+                <Card.Description>
+                  - Led analytics and insights for all Greenlight Hubs in the region using SQL and data-driven experimentation, resulting in better tracking of customer satisfaction and increased NPS to a regional average of above 60 <br />
+                  - Standardized processes across the region, including consolidating Greenlights’ payments system, as well as aggregating and triaging product bugs and driver-partner feedback, further increasing customer satisfaction
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Fade>
+        </div>
+
+
     </div>
   )
 }
 
 export default Experience;
+
+
+        // <Item.Group>
+        //   <Item>
+        //     <Item.Image size='tiny' src='https://pbs.twimg.com/profile_images/694191024416112642/VtJUhbKk_400x400.png' />
+        //     <Item.Content>
+        //       <Item.Header as='a'>Fullstack Academy of Code</Item.Header>
+        //       <Item.Meta>Software Engineering Teaching Fellow</Item.Meta>
+        //       <Item.Description>
+        //         - Provide coaching and technical instruction to 7 students as their assigned teaching fellow <br />
+        //         - Support students in need of troubleshooting assistance and review of programming concepts during workshops <br />
+        //         - Lead technical and behavioral admissions interviews for prospective students
+        //       </Item.Description>
+        //     </Item.Content>
+        //   </Item>
+        //   <Item>
+        //     <Item.Image size='tiny' src='https://pbs.twimg.com/profile_images/694191024416112642/VtJUhbKk_400x400.png' />
+        //     <Item.Content>
+        //       <Item.Header as='a'>Uber</Item.Header>
+        //       <Item.Meta>Regional Greenlight Program Specialist</Item.Meta>
+        //       <Item.Description>
+        //         - Provide coaching and technical instruction to 7 students as their assigned teaching fellow <br />
+        //         - Support students in need of troubleshooting assistance and review of programming concepts during workshops <br />
+        //         - Lead technical and behavioral admissions interviews for prospective students
+        //       </Item.Description>
+        //     </Item.Content>
+        //   </Item>
+        // </Item.Group>
--- src/Intro.js
+++ src/Intro.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react';
 
 const Intro = () => (
   <div className="intro-container">
-    <img src={window.location.origin + '/assets/headshot_casual.jpg'} className="intro-image"></img>
+    <div className="clearfix">
+      <img src={window.location.origin + '/assets/headshot_casual.jpg'} className="intro-image clearfix"></img>
+    </div>
     <div>
       <h2>Hey, I'm Yoni Slotwiner</h2>
       <p className="intro-text">I'm a software developer passionate about the intersection of product, operations, and customer service. I'm particularly interested in developing technology that improves people's lives.</p>
       <p className="intro-text">For the past few years I worked at Uber, where I helped lead the regional Greenlight Hubs (in-person support centers for drivers). While there I became increasingly interested in the technology side of the operation, which is why I eventually decided to leave the company and dedicate myself to learning software development.</p>
       <p className="intro-text">Since then, I've completely immersed myself in the programming world, and every day I look forward to building, learning, and collaborating.</p>
-      <p className="intro-text">Keep scrolling to learn more about me.</p>
+      <h3><Icon name="code"></Icon>Technologies I use</h3>
+      <p className="intro-text">Javascript, Node.js, React.js, Redux, HTML5, CSS, Express, PostgreSQL</p>
     </div>
   </div>
 )
