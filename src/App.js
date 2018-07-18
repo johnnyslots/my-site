3de515cc89f19dbd48fea3f298667e7d3fd899ec
Yoni Slotwinere <yonislots@gmail.com>
2018-07-18 13:07:23 -0400

    landing screen

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
     return (
       <div className="app">
         <Landing />
-
         <Intro />
         <Experience />
       </div>
--- src/Landing.js
+++ src/Landing.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
+import { Icon } from 'semantic-ui-react';
 
 const Landing = () => (
   <div className="landing-container">
-    <h4 className="header-initials-landing">YS</h4>
+    <Navbar />
+    <div className="landing-main-container">
+      <h3 className="header-initials-landing fa fa-arrow-down fa-2x">Yoni Slotwiner</h3>
+      <h5 className="landing-subtitle">Full Stack Web Developer</h5>
+    </div>
+    <div className="arrow bounce">
+      <Icon name="arrow down" size="big"></Icon>
+    </div>
   </div>
 )
 
