089f242e39df1bbb1b3b113e457435da7573a3bf
Yoni Slotwinere <yonislots@gmail.com>
2018-07-18 11:25:20 -0400

    landing screen experiment

---
+++ src/Landing.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Landing = () => (
+  <div className="landing-container">
+    <h4 className="header-initials-landing">YS</h4>
+  </div>
+)
+
+export default Landing;
