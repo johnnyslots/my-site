d08e0a44de8988077b1d0b120648a6c9d9eef6e4
Yoni Slotwinere <yonislots@gmail.com>
2018-07-19 13:23:31 -0400

    collapsible cards rendering all at once

--- src/Experience.js
+++ src/Experience.js
@@ -4,6 +4,44 @@ import { Image, Reveal, Button, Item } from 'semantic-ui-react'
 import Fade from 'react-reveal/Fade';
 import Collapsible from 'react-collapsible';
 
+const experienceDetails = [
+  {
+    company: 'Fullstack Academy of Code',
+    position: 'Software Engineering Teaching Fellow',
+    description: `- Provide coaching and technical instruction to 7 students as their assigned teaching fellow \n - Support students in need of troubleshooting assistance and review of programming concepts during workshops \n - Lead technical and behavioral admissions interviews for prospective students`,
+    imageURL: 'https://pbs.twimg.com/profile_images/694191024416112642/VtJUhbKk_400x400.png',
+    date: 'June 2018 - present'
+  },
+  {
+    company: 'Uber',
+    position: 'Subregional Greenlight Program Specialist',
+    description: '- Provide coaching and technical instruction to 7 students as their assigned teaching fellow \n - Support students in need of troubleshooting assistance and review of programming concepts during workshops \n - Lead technical and behavioral admissions interviews for prospective students',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Uber_App_Icon.svg/1200px-Uber_App_Icon.svg.png',
+    date: 'Aug 2017 - Feb 2018'
+  },
+  {
+    company: 'Uber',
+    position: 'Greenlight Program Manager',
+    description: '- Provide coaching and technical instruction to 7 students as their assigned teaching fellow \n - Support students in need of troubleshooting assistance and review of programming concepts during workshops \n - Lead technical and behavioral admissions interviews for prospective students',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Uber_App_Icon.svg/1200px-Uber_App_Icon.svg.png',
+    date: 'Sep 2015 - Aug 2017'
+  },
+  {
+    company: 'Uber',
+    position: 'Operations Coordinator',
+    description: '- Provide coaching and technical instruction to 7 students as their assigned teaching fellow \n - Support students in need of troubleshooting assistance and review of programming concepts during workshops \n - Lead technical and behavioral admissions interviews for prospective students',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Uber_App_Icon.svg/1200px-Uber_App_Icon.svg.png',
+    date: 'Jun 2014 - Sep 2015'
+  },
+  {
+    company: 'SoundStreak',
+    position: 'Customer Operations & Sales Engineer',
+    description: '- Provide coaching and technical instruction to 7 students as their assigned teaching fellow \n - Support students in need of troubleshooting assistance and review of programming concepts during workshops \n - Lead technical and behavioral admissions interviews for prospective students',
+    imageURL: 'http://cdn2.btrstatic.com/pics/showpics/large/268027_tj6KheHb.png',
+    date: 'Jan 2011 - Jun 2014'
+  }
+]
+
 
 export default class Experience extends Component {
   constructor() {
@@ -25,55 +63,41 @@ export default class Experience extends Component {
     return (
       <div className="experience-container">
         <h2>Experience</h2>
-          <div className="card-left">
-            <Fade cascade right>
-              <Card fluid onClick={this.handleClick}>
-                <Card.Content>
-                  <Image floated='right' size='mini' src='https://pbs.twimg.com/profile_images/694191024416112642/VtJUhbKk_400x400.png' />
-                  <Card.Header>Fullstack Academy of Code</Card.Header>
-                  <Card.Meta>Software Engineering Teaching Fellow</Card.Meta>
-                  <div className={moreDetailsClass}>
-                  <Card.Description>
-                  - Provide coaching and technical instruction to 7 students as their assigned teaching fellow <br />
-                  - Support students in need of troubleshooting assistance and review of programming concepts during workshops <br />
-                  - Lead technical and behavioral admissions interviews for prospective students
-                  </Card.Description>
+
+        {
+          experienceDetails.map((el, i) => {
+            let classNameSide = i % 2 ? 'card-right' : 'card-left'
+            let cascadeLeft, cascadeRight
+            if(i % 2) cascadeLeft = true
+            else cascadeRight = true
+
+            return (
+              <div className={classNameSide}>
+                <Fade cascade left={cascadeLeft} right={cascadeRight}>
+                  <div className='experience-card-container'>
+                    <Card fluid onClick={this.handleClick}>
+                      <Card.Content>
+                        <Image floated='right' size='mini' src={el.imageURL} />
+                        <Card.Header>{el.company}</Card.Header>
+                        <Card.Meta>{el.position}</Card.Meta>
+                        <div className={moreDetailsClass}>
+                          <Card.Description>
+                            <div className='display-linebreak experience-description'>
+                              {el.description}
+                            </div>
+                          </Card.Description>
+                        </div>
+                      </Card.Content>
+                      <Card.Content extra>
+                        {el.date}
+                      </Card.Content>
+                    </Card>
                   </div>
-                </Card.Content>
-              </Card>
-            </Fade>
-          </div>
-          <div className="card-container card-right">
-            <Fade left>
-              <Card fluid>
-                <Card.Content>
-                  <Image floated='right' size='mini' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Uber_App_Icon.svg/1200px-Uber_App_Icon.svg.png' />
-                  <Card.Header>Uber</Card.Header>
-                  <Card.Meta>Subregional Greenlight Program Specialist</Card.Meta>
-                  <Card.Description>
-                    - Led analytics and insights for all Greenlight Hubs in the region using SQL and data-driven experimentation, resulting in better tracking of customer satisfaction and increased NPS to a regional average of above 60 <br />
-                    - Standardized processes across the region, including consolidating Greenlights’ payments system, as well as aggregating and triaging product bugs and driver-partner feedback, further increasing customer satisfaction <br />
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            </Fade>
-          </div>
-          <div className="card-left">
-            <Fade cascade right>
-              <Card fluid>
-                <Card.Content>
-                  <Image floated='right' size='mini' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Uber_App_Icon.svg/1200px-Uber_App_Icon.svg.png' />
-                  <Card.Header>Uber</Card.Header>
-                  <Card.Meta>Greenlight Program Manager</Card.Meta>
-                  <Card.Description>
-                    - Oversaw and managed 3 Greenlight Hubs across 3 boroughs, with a total staff of 60 employees serving approximately 5,000 driver-partners per week, and a budget totaling $300k per month <br />
-                    - Created and maintained a staffing model to identify inefficiencies in our scheduling, resulting in changes to our operating hours and minimized overtime for support representatives <br />
-                    - Created and maintained a staffing model to identify inefficiencies in our scheduling, resulting in changes to our operating hours and minimized overtime for support representatives <br />
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            </Fade>
-          </div>
+                </Fade>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,6 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <h4 className="header-initials">YS</h4>
-      <div className="nav-links-container">
-        <a className="nav-item">projects</a>
-        <a className="nav-item">experience</a>
-        <a className="nav-item">contact</a>
-      </div>
     </div>
   )
 }
