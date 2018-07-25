a27d5b66fb65261405a88cef8f72b62d4d00e9db
Yoni Slotwinere <yonislots@gmail.com>
2018-07-24 22:06:43 -0400

    project section styling; missing tech icons

--- src/Projects.js
+++ src/Projects.js
@@ -16,11 +16,11 @@ const Projects = () => {
           <div>
             <h2>Code Bono</h2>
             <p>Code Bono is a platform that connects nonprofit organizations with software developers looking to contribute their skills through pro bono work.</p>
-            <h4 className='projects-visit'><a className='projects-visit-link' href='http://www.codebono.org/'>VISIT</a></h4>
+            <h4 className='projects-visit'><a target="_blank" className='projects-visit-link' href='http://www.codebono.org/'>VISIT</a></h4>
             <div className='projects-links-border'>
               <Icon name='sitemap' size='small'></Icon>
             </div>
-            <h4 className='projects-github'><a className='projects-visit-link' href='http://www.codebono.org/'>GITHUB</a></h4>
+            <h4 className='projects-github'><a target="_blank" className='projects-visit-link' href='https://github.com/Code-Bono/code-bono'>GITHUB</a></h4>
           </div>
           <div className='projects-image-container'>
             <img src={window.location.origin + '/assets/code-bono-mac.png'} className='projects-image'></img>
@@ -33,25 +33,25 @@ const Projects = () => {
           <div>
             <h2>CheckIt</h2>
             <p>A mobile app that allows you to send and receive recommendations for content such as books, movies, podcasts, and more. You can keep track of who recommended what and when, manage your lists, and get real-time notifications for new recommendation.</p>
-            <h4 className='projects-visit'><a className='projects-visit-link' href='https://expo.io/@yonislots/checkit-app'>VISIT</a></h4>
+            <h4 className='projects-visit'><a target="_blank" className='projects-visit-link' href='https://expo.io/@yonislots/checkit-app'>VISIT</a></h4>
             <div className='projects-links-border'>
               <Icon name='sitemap' size='small'></Icon>
             </div>
-            <h4 className='projects-github'><a className='projects-visit-link' href='https://expo.io/@yonislots/checkit-app'>GITHUB</a></h4>
+            <h4 className='projects-github'><a target="_blank" className='projects-visit-link' href='https://github.com/johnnyslots/checkit-app'>GITHUB</a></h4>
           </div>
         </div>
-        <div className='projects-item-container'>
+        <div className=' projects-item-container no-border'>
           <div>
             <h2>D&D Wine and Liquor</h2>
-            <p>A mock e-commerce site that sells a selection of wine and liquor.</p>
-            <h4 className='projects-visit'><a className='projects-visit-link' href='https://expo.io/@yonislots/checkit-app'>VISIT</a></h4>
+            <p>A mock e-commerce site that sells a selection of wine and liquor. This project also incorporates the Stripe API for a fully-functioning check-out experience.</p>
+            <h4 className='projects-visit'><a target="_blank" className='projects-visit-link' href='https://dd-wine.herokuapp.com/'>VISIT</a></h4>
             <div className='projects-links-border'>
               <Icon name='sitemap' size='small'></Icon>
             </div>
-            <h4 className='projects-github'><a className='projects-visit-link' href='https://expo.io/@yonislots/checkit-app'>GITHUB</a></h4>
+            <h4 className='projects-github'><a target="_blank" className='projects-visit-link' href='https://github.com/dd-grace-shopper/dd-grace-shopper'>GITHUB</a></h4>
           </div>
           <div className='projects-image-container'>
-            <img src={window.location.origin + '/assets/checkit-iphone.png'} className='projects-image'></img>
+            <img src={window.location.origin + '/assets/dd-wine.png'} className='projects-image-laptop'></img>
           </div>
         </div>
       </div>
